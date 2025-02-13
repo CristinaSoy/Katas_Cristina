fc3afaae5432ea0c882335ae6bee0e47a3bc51f6
CristinaSoy <ccardonaaltes@hotmail.com>
2025-02-13 13:52:34 +0100

    Feb _13

---
+++ Feb_06/index.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+$n = 2;
+$alumnes = [
+    "Pere" => [1],
+    "Joan" => [1],
+    "Jesus" => [1],
+    "Mayte" => [1],
+    "Julia" =>[1],
+];
+
+//crea tabla con todos los grupos
+range(1,$n)
+echo current ($array)
+array_keys($array, "Joan")
+
+foreach("alumnes" as $alumne => $grup) {
+    $grupAsignat = &$rand(1,$n);
+}
+
+for ($i=0; $<count($alumnes); $i++) {
+
+}
+
+
+foreach("alumnes" as $alumne => $grup) {
+      echo "Grup";
+}
+
+
+Grup 1: Joan Julia
+Grup 2: Pere Joan Mayte
+?>
---
+++ Feb_06/index2.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+$n = 2; 
+$alumnes = ["Pere", "Joan", "Jesus", "Mayte", "Julia"];
+
+function distribuirAlumnosEnGrupos(array $alumnes, int $n) {
+    
+    shuffle($alumnes);
+    $grups = array_fill(1, $n, []); // Inicializa los grupos
+    echo "Grups: ";
+    print_r($grups);
+    echo "<br>";
+
+    foreach ($alumnes as $index => $alumno) {
+        $grupoIndex = ($index % $numGrupos) + 1; // Distribuye en grupos cíclicamente
+        $grupos[$grupoIndex][] = $alumno;
+    }
+
+    // Imprimir los grupos
+    foreach ($grupos as $grupo => $integrantes) {
+        echo "Grupo $grupo: " . implode(", ", $integrantes) . PHP_EOL;
+    }
+}
+
+// Definir alumnos y número de grupos
+$alumnos = ["Pedro", "Luis", "Carmen", "Laura", "Ana", "Jose"]; // Seis alumnos en este caso
+$n = 2; // Número de grupos
+
+distribuirAlumnosEnGrupos($alumnos, $n);
+?>
