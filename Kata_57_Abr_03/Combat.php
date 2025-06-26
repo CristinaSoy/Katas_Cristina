de9f038493098097bc1cf16c977546f052973b0a
CristinaSoy <ccardonaaltes@hotmail.com>
2025-06-26 13:46:00 +0200

    kata 57 seguimos con planteamiento

--- Kata_57_Abr_03/Combat.php
+++ Kata_57_Abr_03/Combat.php
@@ -4,13 +4,15 @@ use Personaje;
 
 class Combat {
 
+
+
     private array $personajes;
 
-    public function __construct() {
+    public function __construct(array $personajes) {
         
-        //Crear els tres personatjes que s'enfronten en combat
-    $pers1 = new Personaje("Isabel", 10500, 13520, ['volar', 'sanar', 'reir']);
-    $pers2 = new Personaje("Paco", 1500, 3520, ['gritar', 'ganar dinero', 'volar']);
-    $pers3 = new Personaje("Lola", 1200, 73520, ['votar', 'escribir', 'cantar']);
-    }
+
+
+    // guarda els personatjes en un array:
+
+
 }
--- Kata_57_Abr_03/index.php
+++ Kata_57_Abr_03/index.php
@@ -4,3 +4,11 @@
 
 
 
+// Creo el combate y le inyecto los 3 personajes
+$combat = new Combat(
+$personaje1 = new Personaje("Isabel", 10500, 13520, ['volar', 'sanar', 'reir']):
+   new Personaje("Paco", 1500, 3520, ['gritar', 'ganar dinero', 'volar']),
+   new Personaje("Lola", 1200, 73520, ['votar', 'escribir', 'cantar']),
+);
+
+//Guardo los personajes en un array
